ce24bff2203e5a7f4930cc67ac0d9b85a7db334d
Dawid Lubowiecki <dawid.lubowiecki@opi.org.pl>
2023-09-11 11:40:11 +0200

    astro 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.reksio-wet.pl",
-  integrations: [
-    tailwind(),
-    sitemap(),
-    compress({
-      img: false,
-      svg: false,
-      css: false,
-      js: false,
-    }),
-  ],
+  integrations: [tailwind(), sitemap()],
+  compressHTML: true,
 });
